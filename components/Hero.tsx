5831f62e43a9d0be40825703e8fdffddeefa51ce
Nitesh000 <ntudu040@gmail.com>
2022-12-27 15:30:53 +0530

    feat(hero backgroundcircles): done with both of this and now the rest of the parts

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,12 @@
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Image from "next/image";
+import my_image from "../public/my_image.png";
 
 const Hero = () => {
   const [text, count] = useTypewriter({
     words: [
-      "Hii, @Nitesh here",
+      "Hii, Nitesh here",
       "loves-entertainment-and-comedy",
       "<ButLovesCodingMore />",
     ],
@@ -11,11 +14,28 @@ const Hero = () => {
     delaySpeed: 2000,
   });
   return (
-    <div>
-      <h1>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <Image
+        src={my_image}
+        alt="picture of the creator"
+        width={140}
+        height={140}
+        className="relative rounded-full"
+      />
+      <h2 className="text-sm uppercase tracking-[15px] text-gray-500 pb-2">
+        Software Engineer
+      </h2>
+      <h1 className="text-5xl lg:text-6xl font-semibold px-10">
         <span>{text}</span>
         <Cursor cursorColor="#F7AB0A" />
       </h1>
+      <div>
+        <button className="heroButton">About</button>
+        <button className="heroButton">Experience</button>
+        <button className="heroButton">Skills</button>
+        <button className="heroButton">Projects</button>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,12 @@ import Hero from "../components/Hero";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll ">
       {/* Header */}
       <Header />
 
       {/* Hero */}
-      <section id="hero">
+      <section id="hero" className="snap-center">
         <Hero />
       </section>
 
