993f7b20bdf07b02408bfea3ac7d9d5cf9226cc7
Nitesh000 <ntudu040@gmail.com>
2023-01-05 17:00:28 +0530

    chore(sanity): change it to version 3
    I was writing the code in version 2 so i changed it to the version 3.

---
+++ nitesh-portfolio/schemas/experience.js
@@ -0,0 +1,54 @@
+import {defineType, defineField} from 'sanity'
+
+export default defineType({
+  name: 'experience',
+  title: 'Experience',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'jobTitle',
+      title: 'JobTitle',
+      type: 'string',
+    }),
+    defineField({
+      name: 'companyImage',
+      title: 'Company Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'company',
+      title: 'Company',
+      type: 'text',
+    }),
+    defineField({
+      name: 'dateStarted',
+      title: 'DateStarted',
+      type: 'date',
+    }),
+    defineField({
+      name: 'dateEnded',
+      title: 'DateEnded',
+      type: 'date',
+    }),
+    defineField({
+      name: 'isCurrentlyWrokingHere',
+      title: 'IsCurrentlyWrokingHere',
+      type: 'boolean',
+    }),
+    defineField({
+      name: 'technologies',
+      title: 'Technologies',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'skill'}}],
+    }),
+    defineField({
+      name: 'points',
+      title: 'Points',
+      type: 'array',
+      of: [{type: 'string'}],
+    }),
+  ],
+})
--- nitesh-portfolio/schemas/index.js
+++ nitesh-portfolio/schemas/index.js
@@ -1,6 +1,7 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import experience from './experience'
+import pageInfo from './pageInfo'
+import project from './project'
+import skill from './skill'
+import social from './social'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [experience, pageInfo, project, skill, social]
